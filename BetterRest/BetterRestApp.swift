63be84671a15e8f2aee89a667349b4ef113a8541
aidanbergerson <aidan.bergerson@gmail.com>
2025-01-05 16:20:03 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aidan Bergerson on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
